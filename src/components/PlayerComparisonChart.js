7e4f8f4b197d088670616cd1a30066e62cd8fe12
Aditya <adityasingh220598@gmail.com>
2023-04-17 09:32:22 +0530

    Individual titles plotting

--- src/components/PlayerComparisonChart.js
+++ src/components/PlayerComparisonChart.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import cheerio from "cheerio";
 import * as d3 from "d3";
 
-const PlayerCompareisonChart = () => {
+const PlayerComparisonChart = () => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
@@ -22,19 +22,136 @@ const PlayerCompareisonChart = () => {
           .eq(1)
           .text()
       );
-      console.log(ronaldoGoals);
-      console.log(messiGoals);
+
+      const resultHonors = await axios.get(
+        "/api/cors?url=https://www.messivsronaldo.app/honours-and-achievements/"
+      );
+      const $honors = cheerio.load(resultHonors.data);
+
+      const parameters = [
+        "Champions League",
+        "League Titles",
+        "Domestic Cup",
+        "Domestic Super Cup",
+        "UEFA Super Cup",
+        "Club World Cup",
+        "Full Senior International",
+      ];
+
+      const scrapedData = parameters.map((parameter) => {
+        const article = $honors(`h2:contains("${parameter}")`).parent();
+        const messiValue = parseInt(
+          article
+            .find(".DetailedStatsPageBlock-module--mainStat--Opo-J")
+            .first()
+            .text()
+        );
+        const ronaldoValue = parseInt(
+          article
+            .find(".DetailedStatsPageBlock-module--mainStat--Opo-J")
+            .eq(1)
+            .text()
+        );
+        return { parameter, messiValue, ronaldoValue };
+      });
+
       setData([
-        { name: "Messi", goals: messiGoals },
-        { name: "Ronaldo", goals: ronaldoGoals },
+        {
+          parameter: "GOALS",
+          messiValue: messiGoals,
+          ronaldoValue: ronaldoGoals,
+        },
+        ...scrapedData,
       ]);
     };
     fetchData();
   }, []);
 
-  // logic to plot data using d3.js
   useEffect(() => {
     if (data.length === 0) return;
+    renderLineChart(data);
+  }, [data]);
+
+  useEffect(() => {
+    if (data.length === 0) return;
+    data.forEach((item, index) => renderChart(item, index));
+  }, [data]);
+
+  const renderLineChart = (data) => {
+    // set the dimensions and margins of the graph
+    const margin = { top: 30, right: 40, bottom: 50, left: 60 },
+      width = 960 - margin.left - margin.right,
+      height = 500 - margin.top - margin.bottom;
+
+    // set the ranges
+    const x = d3.scalePoint().range([0, width]).padding(0.5);
+    const y = d3.scaleLinear().range([height, 0]);
+
+    d3.select("#lineChart").select("svg").remove();
+
+    const svg = d3
+      .select("#lineChart")
+      .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom + 50)
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    // Scale the range of the data in the domains
+    x.domain(data.map((d) => d.parameter));
+    y.domain([0, d3.max(data, (d) => Math.max(d.messiValue, d.ronaldoValue))]);
+
+    // Define the lines
+    const messiLine = d3
+      .line()
+      .x((d) => x(d.parameter))
+      .y((d) => y(d.messiValue));
+
+    const ronaldoLine = d3
+      .line()
+      .x((d) => x(d.parameter))
+      .y((d) => y(d.ronaldoValue));
+
+    // Add the Messi line
+    svg
+      .append("path")
+      .datum(data)
+      .attr("class", "line")
+      .attr("d", messiLine)
+      .style("stroke", "blue")
+      .style("stroke-width", 2)
+      .style("fill", "none");
+
+    // Add the Ronaldo line
+    svg
+      .append("path")
+      .datum(data)
+      .attr("class", "line")
+      .attr("d", ronaldoLine)
+      .style("stroke", "red")
+      .style("stroke-width", 2)
+      .style("fill", "none");
+
+    // Add the X Axis
+    svg
+      .append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x))
+      .selectAll("text")
+      .attr("transform", "rotate(-45)")
+      .style("text-anchor", "end");
+
+    // Add the Y Axis
+    svg.append("g").call(d3.axisLeft(y));
+  };
+
+  const renderChart = (item, index) => {
+    const { parameter, messiValue, ronaldoValue } = item;
+
+    const playerData = [
+      { name: "Messi", value: messiValue },
+      { name: "Ronaldo", value: ronaldoValue },
+    ];
 
     // set the dimensions and margins of the graph
     const margin = { top: 30, right: 40, bottom: 50, left: 60 },
@@ -45,14 +162,11 @@ const PlayerCompareisonChart = () => {
     const x = d3.scaleBand().range([0, width]).padding(0.5);
     const y = d3.scaleLinear().range([height, 0]);
 
-    // append the svg obgect to the body of the page
-    // appends a 'group' element to 'svg'
-    // moves the 'group' element to the top left margin
-    d3.select("#chart").select("svg").remove();
-    const tooltip = d3.select("#tooltip");
+    d3.select(`#chart${index}`).select("svg").remove();
+    const tooltip = d3.select(`#tooltip${index}`);
 
     const svg = d3
-      .select("#chart")
+      .select(`#chart${index}`)
       .append("svg")
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom)
@@ -60,12 +174,12 @@ const PlayerCompareisonChart = () => {
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
     // Scale the range of the data in the domains
-    x.domain(data.map((d) => d.name));
-    y.domain([0, d3.max(data, (d) => d.goals)]);
+    x.domain(playerData.map((d) => d.name));
+    y.domain([0, d3.max(playerData, (d) => d.value)]);
 
     svg
       .selectAll(".bar")
-      .data(data)
+      .data(playerData)
       .enter()
       .append("rect")
       .attr("class", "bar")
@@ -77,7 +191,7 @@ const PlayerCompareisonChart = () => {
       .on("mouseenter", (event, d) => {
         tooltip
           .style("opacity", 1)
-          .html(`Goals: ${d.goals}`)
+          .html(`${parameter}: ${d.value}`)
           .style("background-color", d.name === "Messi" ? "blue" : "red")
           .style("color", "white")
           .style("padding", "4px");
@@ -92,25 +206,8 @@ const PlayerCompareisonChart = () => {
       })
       .transition()
       .duration(1000)
-      .attr("y", (d) => y(d.goals))
-      .attr("height", (d) => height - y(d.goals));
-
-    // append the rectangles for the bar chart
-    svg
-      .selectAll(".bar")
-      .data(data)
-      .enter()
-      .append("rect")
-      .attr("class", "bar")
-      .attr("x", (d) => x(d.name))
-      .attr("width", x.bandwidth())
-      .attr("y", (d) => y(0))
-      .attr("height", 0)
-      .style("fill", (d) => (d.name === "Messi" ? "blue" : "red"))
-      .transition()
-      .duration(1000)
-      .attr("y", (d) => y(d.goals))
-      .attr("height", (d) => height - y(d.goals));
+      .attr("y", (d) => y(d.value))
+      .attr("height", (d) => height - y(d.value));
 
     // add the x Axis
     svg
@@ -131,17 +228,6 @@ const PlayerCompareisonChart = () => {
     // add the y Axis
     svg.append("g").call(d3.axisLeft(y));
 
-    const legend = svg
-      .append("g")
-      .attr("font-family", "sans-serif")
-      .attr("font-size", 10)
-      .attr("text-anchor", "end")
-      .selectAll("g")
-      .data(data)
-      .enter()
-      .append("g")
-      .attr("transform", (d, i) => "translate(0," + i * 20 + ")");
-
     // add the y Axis label
     svg
       .append("text")
@@ -150,29 +236,23 @@ const PlayerCompareisonChart = () => {
       .attr("x", 0 - height / 2)
       .attr("dy", "1em")
       .style("text-anchor", "middle")
-      .text("Number of goals");
-
-    legend
-      .append("rect")
-      .attr("x", width - 19)
-      .attr("width", 19)
-      .attr("height", 19)
-      .style("fill", (d) => (d.name === "Messi" ? "blue" : "red"));
-
-    legend
-      .append("text")
-      .attr("x", width - 24)
-      .attr("y", 9.5)
-      .attr("dy", "0.32em")
-      .text((d) => `Goals for ${d.name}`);
-  }, [data]);
+      .text(parameter);
+  };
 
   return (
     <div>
-      <div id="chart"></div>
-      <div id="tooltip" style={{ opacity: 0, position: "absolute" }}></div>
+      {data.map((_, index) => (
+        <React.Fragment key={index}>
+          <div id={`chart${index}`}></div>
+          <div
+            id={`tooltip${index}`}
+            style={{ opacity: 0, position: "absolute" }}
+          ></div>
+        </React.Fragment>
+      ))}
+      <div id="lineChart"></div>
     </div>
   );
 };
 
-export default PlayerCompareisonChart;
+export default PlayerComparisonChart;
