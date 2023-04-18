fe3b9062714e587a21f30b8ea3ff29426321861a
Aditya <adityasingh220598@gmail.com>
2023-04-18 18:06:53 +0530

    Deploying histograms

--- src/components/PlayerComparisonChart.js
+++ src/components/PlayerComparisonChart.js
@@ -69,104 +69,25 @@ const PlayerComparisonChart = () => {
 
   useEffect(() => {
     if (data.length === 0) return;
-    renderLineChart(data);
+    renderHistogram(data);
+    renderHorizontalHistogram(data);
   }, [data]);
 
-  useEffect(() => {
-    if (data.length === 0) return;
-    data.forEach((item, index) => renderChart(item, index));
-  }, [data]);
-
-  const renderLineChart = (data) => {
+  const renderHistogram = (data) => {
     // set the dimensions and margins of the graph
     const margin = { top: 30, right: 40, bottom: 50, left: 60 },
       width = 960 - margin.left - margin.right,
       height = 500 - margin.top - margin.bottom;
 
     // set the ranges
-    const x = d3.scalePoint().range([0, width]).padding(0.5);
-    const y = d3.scaleLinear().range([height, 0]);
+    const x0 = d3.scaleBand().range([0, width]).paddingInner(0.2);
+    const x1 = d3.scaleBand().padding(0.05);
+    const y = d3.scaleLog().base(2).range([height, 0]).clamp(true);
 
-    d3.select("#lineChart").select("svg").remove();
-
-    const svg = d3
-      .select("#lineChart")
-      .append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom + 50)
-      .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    // Scale the range of the data in the domains
-    x.domain(data.map((d) => d.parameter));
-    y.domain([0, d3.max(data, (d) => Math.max(d.messiValue, d.ronaldoValue))]);
-
-    // Define the lines
-    const messiLine = d3
-      .line()
-      .x((d) => x(d.parameter))
-      .y((d) => y(d.messiValue));
-
-    const ronaldoLine = d3
-      .line()
-      .x((d) => x(d.parameter))
-      .y((d) => y(d.ronaldoValue));
-
-    // Add the Messi line
-    svg
-      .append("path")
-      .datum(data)
-      .attr("class", "line")
-      .attr("d", messiLine)
-      .style("stroke", "blue")
-      .style("stroke-width", 2)
-      .style("fill", "none");
-
-    // Add the Ronaldo line
-    svg
-      .append("path")
-      .datum(data)
-      .attr("class", "line")
-      .attr("d", ronaldoLine)
-      .style("stroke", "red")
-      .style("stroke-width", 2)
-      .style("fill", "none");
-
-    // Add the X Axis
-    svg
-      .append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x))
-      .selectAll("text")
-      .attr("transform", "rotate(-45)")
-      .style("text-anchor", "end");
-
-    // Add the Y Axis
-    svg.append("g").call(d3.axisLeft(y));
-  };
-
-  const renderChart = (item, index) => {
-    const { parameter, messiValue, ronaldoValue } = item;
-
-    const playerData = [
-      { name: "Messi", value: messiValue },
-      { name: "Ronaldo", value: ronaldoValue },
-    ];
-
-    // set the dimensions and margins of the graph
-    const margin = { top: 30, right: 40, bottom: 50, left: 60 },
-      width = 960 - margin.left - margin.right,
-      height = 500 - margin.top - margin.bottom;
-
-    // set the ranges
-    const x = d3.scaleBand().range([0, width]).padding(0.5);
-    const y = d3.scaleLinear().range([height, 0]);
-
-    d3.select(`#chart${index}`).select("svg").remove();
-    const tooltip = d3.select(`#tooltip${index}`);
+    d3.select("#chart").select("svg").remove();
 
     const svg = d3
-      .select(`#chart${index}`)
+      .select("#chart")
       .append("svg")
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom)
@@ -174,40 +95,133 @@ const PlayerComparisonChart = () => {
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
     // Scale the range of the data in the domains
-    x.domain(playerData.map((d) => d.name));
-    y.domain([0, d3.max(playerData, (d) => d.value)]);
+    x0.domain(data.map((d) => d.parameter));
+    x1.domain(["Messi", "Ronaldo"]).range([0, x0.bandwidth() / data.length]);
+    y.domain([
+      1,
+      d3.max(data, (d) => Math.max(d.messiValue, d.ronaldoValue) + 1),
+    ]);
 
+    data.forEach((item) => {
+      const { parameter, messiValue, ronaldoValue } = item;
+      const playerData = [
+        { name: "Messi", value: messiValue },
+        { name: "Ronaldo", value: ronaldoValue },
+      ];
+
+      svg
+        .append("g")
+        .selectAll("g")
+        .data([playerData])
+        .enter()
+        .append("g")
+        .attr("transform", (d) => `translate(${x0(parameter)},0)`)
+        .selectAll("rect")
+        .data((d) => d)
+        .enter()
+        .append("rect")
+        .attr("x", (d) => x1(d.name))
+        .attr("y", (d) => y(d.value))
+        .attr("width", x1.bandwidth())
+        .attr("height", (d) => height - y(d.value))
+        .attr("fill", (d) => (d.name === "Messi" ? "blue" : "red"));
+    });
+
+    // add the x Axis
     svg
-      .selectAll(".bar")
-      .data(playerData)
-      .enter()
-      .append("rect")
-      .attr("class", "bar")
-      .attr("x", (d) => x(d.name))
-      .attr("width", x.bandwidth())
-      .attr("y", (d) => y(0))
-      .attr("height", 0)
-      .style("fill", (d) => (d.name === "Messi" ? "blue" : "red"))
-      .on("mouseenter", (event, d) => {
-        tooltip
-          .style("opacity", 1)
-          .html(`${parameter}: ${d.value}`)
-          .style("background-color", d.name === "Messi" ? "blue" : "red")
-          .style("color", "white")
-          .style("padding", "4px");
-      })
-      .on("mousemove", (event) => {
-        tooltip
-          .style("left", event.pageX + "px")
-          .style("top", event.pageY - 28 + "px");
-      })
-      .on("mouseleave", () => {
-        tooltip.style("opacity", 0);
-      })
-      .transition()
-      .duration(1000)
-      .attr("y", (d) => y(d.value))
-      .attr("height", (d) => height - y(d.value));
+      .append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x0));
+
+    // add the x Axis label
+    svg
+      .append("text")
+      .attr(
+        "transform",
+        "translate(" + width / 2 + "," + (height + margin.bottom / 1.5) + ")"
+      )
+      .style("text-anchor", "middle")
+      .text("Parameter");
+    // add the y Axis
+    svg.append("g").call(d3.axisLeft(y));
+
+    // add the y Axis label
+    svg
+      .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left)
+      .attr("x", 0 - height / 2)
+      .attr("dy", "1em")
+      .style("text-anchor", "middle")
+      .text("Value");
+  };
+
+  const renderHorizontalHistogram = (data) => {
+    // set the dimensions and margins of the graph
+    const margin = { top: 30, right: 40, bottom: 50, left: 120 },
+      width = 1080 - margin.left - margin.right,
+      height = 500 - margin.top - margin.bottom;
+
+    // set the ranges
+    const y0 = d3.scaleBand().range([0, height]).paddingInner(0.2);
+    const y1 = d3.scaleBand().padding(0.05);
+    const x = d3.scaleLog().base(2).range([0, width]).clamp(true);
+
+    d3.select("#horizontal-chart").select("svg").remove();
+
+    const svg = d3
+      .select("#horizontal-chart")
+      .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    // Scale the range of the data in the domains
+    y0.domain(data.map((d) => d.parameter));
+    y1.domain(["Messi", "Ronaldo"]).range([0, y0.bandwidth() / data.length]);
+    x.domain([
+      1,
+      d3.max(data, (d) => Math.max(d.messiValue, d.ronaldoValue) + 1),
+    ]);
+
+    data.forEach((item) => {
+      const { parameter, messiValue, ronaldoValue } = item;
+      const playerData = [
+        { name: "Messi", value: messiValue },
+        { name: "Ronaldo", value: ronaldoValue },
+      ];
+
+      svg
+        .append("g")
+        .selectAll("g")
+        .data([playerData])
+        .enter()
+        .append("g")
+        .attr("transform", (d) => `translate(0,${y0(parameter)})`)
+        .selectAll("rect")
+        .data((d) => d)
+        .enter()
+        .append("rect")
+        .attr("y", (d) => y1(d.name))
+        .attr("width", (d) => x(d.value))
+        .attr("height", y1.bandwidth())
+        .attr("width", (d) => x(d.value))
+        .attr("fill", (d) => (d.name === "Messi" ? "blue" : "red"));
+    });
+
+    // add the y Axis
+    svg.append("g").call(d3.axisLeft(y0));
+
+    // add the y Axis label
+    svg
+      .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left)
+      .attr("x", 0 - height / 2)
+      .attr("dy", "1em")
+      .style("text-anchor", "middle")
+      .text("Parameter");
 
     // add the x Axis
     svg
@@ -222,35 +236,14 @@ const PlayerComparisonChart = () => {
         "transform",
         "translate(" + width / 2 + "," + (height + margin.bottom / 1.5) + ")"
       )
-      .style("text-anchor", "middle")
-      .text("Player");
-
-    // add the y Axis
-    svg.append("g").call(d3.axisLeft(y));
-
-    // add the y Axis label
-    svg
-      .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 0 - margin.left)
-      .attr("x", 0 - height / 2)
-      .attr("dy", "1em")
-      .style("text-anchor", "middle")
-      .text(parameter);
+      .style(".anchor", "middle")
+      .text("Value");
   };
 
   return (
     <div>
-      {data.map((_, index) => (
-        <React.Fragment key={index}>
-          <div id={`chart${index}`}></div>
-          <div
-            id={`tooltip${index}`}
-            style={{ opacity: 0, position: "absolute" }}
-          ></div>
-        </React.Fragment>
-      ))}
-      <div id="lineChart"></div>
+      <div id="chart"></div>
+      <div id="horizontal-chart"></div>
     </div>
   );
 };
